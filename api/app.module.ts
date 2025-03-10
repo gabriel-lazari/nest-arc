3336bdc423a8133732d4c4971fea4c896c18bd20
NewMiniBoss <gabriel.f.lazari@gmail.com>
2025-03-10 18:54:58 -0300

    chore: configurando banco de dados

--- api/app.module.ts
+++ api/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule]
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+      inject: [DatabaseConfigService]
+    })
+  ]
 })
 
 export class AppModule {}
