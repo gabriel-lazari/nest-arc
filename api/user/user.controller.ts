343da9e4569c2766b5a2aae741fa528d62612dce
NewMiniBoss <gabriel.f.lazari@gmail.com>
2025-03-11 09:53:06 -0300

    chole:"db conectado

--- api/user/user.controller.ts
+++ api/user/user.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { UserEntity} from "./user.entity";
 import { UserRepository } from "./user.repository";
 import { CreateDTO } from "./dto/createDto";
-import { v4 as uuid } from "uuid";
 
 @Controller('/user')
 export class UserController {
@@ -12,13 +11,12 @@ export class UserController {
     async create(@Body() userData: CreateDTO) {
         const userEntity = new UserEntity();
 
-        userEntity.uuid = uuid();
         userEntity.name = userData.name;
         userEntity.email = userData.email;
         userEntity.password = userData.password;
 
         await this.userRepository.create(userEntity);
 
-        return { uuid: userEntity.uuid, message: 'usuario criado com sucesso' };
+        return { message: 'usuario criado com sucesso' };
     }
 }
--- api/user/user.entity.ts
+++ api/user/user.entity.ts
@@ -1,6 +1,25 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'users' })
 export class UserEntity {
-    uuid: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @CreateDateColumn({ name: 'created_at' })
+    created_at: string;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updated_at: string;
+
+    @DeleteDateColumn({ name: 'deleted_at' })
+    deleted_at: string;
+
+    @Column({ name: 'name', length: 100, nullable: false })
     name: string;
+
+    @Column({ name: 'email', length: 70, nullable: false })
     email: string;
+
+    @Column({ name: 'password', length: 255, nullable: false })
     password: string;
 }
