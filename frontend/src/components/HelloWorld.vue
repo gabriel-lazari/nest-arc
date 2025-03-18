92bb5a8d84920bb255be441c591ee629f59de68c
NewMiniBoss <gabriel.f.lazari@gmail.com>
2025-03-18 19:52:58 -0300

    add frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <button @click="showAlert">Mostrar Alerta</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class SideBar extends Vue {}
+</script>
+
+<style scoped lang="scss">
+
+</style>
